2bd3410ced94901836130bf2840e4d0eb837323b
V21-N <vinnr21@gmail.com>
2025-10-15 11:09:02 +0700

    Mengenal View

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // return view('welcome');
-    return "<h1>Saya Alvin</h1>
-    <a href='/about'>About</a><br>
-    <a href='/profil'>Profilr</a><br>"
+     return view('index');
 ;
 });
 
 Route::get('/about', function () {
-    // return view('welcome');
-    return "<h1>Tentang Saya</h1>
-    <a href='/'>Home</a><br>
-    <a href='/profile'>Profile</a><br>"
+     return view('about');
 ;
+});
 
-});Route::get('/profile', function () {
-    // return view('welcome');
-    return "<h1>Profile Saya</h1>
-    <a href='/about'>About</a><br>
-    <a href='/'>Home</a><br>"
+Route::get('/profile', function () {
+     return view('mahasiswa');
+;
+});
+
+Route::get('/profile', function () {
+    $nama = "Alvin";
+    // return view('profile',compact('nama'));
+    return view('profile')->with('nama', $nama)
 ;
 });
