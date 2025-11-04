f5fb077cad1f06ed855142708067137ec8c89645
V21-N <vinnr21@gmail.com>
2025-11-04 14:52:03 +0700

    Sidebar Bootstrap 5

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,51 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>UTS Laravel - Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <style>
+        .brand-judul {
+            font-weight: bold;
+        }
+        .sidebar-menu {
+            background: linear-gradient(135deg, #6a11cb 0%, #2575fc 100%);
+            color: white;
+            padding: 80px 0;
+        }
+        .menu-kartu {
+            transition: transform 0.3s;
+            height: 100%;
+        }
+        .menu-kartu:hover {
+            transform: translateY(-10px);
+        }
+        .bagian-bawah {
+            background-color: #f8f9fa;
+            padding: 25px 0;
+            margin-top: 40px;
+        }
+    </style>
 </head>
 <body>
-    <h1>Hello, Ini adalah halaman HOME</h1>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+     <nav class="navbar bg-light fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand brand-judul" href="#">UTS Laravel</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#menuSamping" aria-controls="menuSamping" aria-label="Buka menu">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+        <div class="container">
+            <h1>Halaman Home</h1>
+            <br>
+        </div>
+    </nav>
+    
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
